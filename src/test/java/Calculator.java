d0ff25578f05667eb841b8d0756df6da51ae64cf
Daria <dashatestit@gmail.com>
2026-06-29 18:24:12 +0300

    Добавлены классы Calculator и CalculatorTest, написаны тесты на калькулятор (5 позитивных, 1 негативный)

---
+++ src/test/java/Calculator.java
@@ -0,0 +1,20 @@
+public class Calculator {
+
+    public static int sum(int a, int b) {
+        return a+b;
+    }
+    public static int subtract(int a, int b) { // вычитание
+        return a-b;
+    }
+
+    public static int multiply (int a, int b) { // умножение
+        return a*b;
+    }
+    public static int divide (int a, int b){ // деление
+        return a/b;
+    }
+
+    public static double average (double a, double b) { // среднее арифметическое
+        return (a+b)/2;
+    }
+}
